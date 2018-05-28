aba22f89a28a16778e744a4639079bfb5efc767a
pawel Salyjan <Salyjan@wp.pl>
2018-05-28 18:24:04 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function calculator() {
+
+    const dolar = 4.87;
+    const euro = 3.75;
+    const nic = 1;
+    const liczba = document.querySelector(".liczba1").value;
+    const waluta = document.querySelector(".currency-1").value;
+    const waluta1 = document.querySelector(".currency-2").value;
+
+    if (liczba < 0)
+        document.getElementById("wynik").innerHTML = "Podana ilość waluty powinna być wartością dodatnią!!!";
+    else if ((waluta == "PLN" && waluta1 == "PLN") || (waluta == "USD" && waluta1 == "USD") || (waluta == "EUR" && waluta1 == "EUR"))
+        document.getElementById("wynik").innerHTML = liczba + waluta + " = " + liczba * nic + waluta1;
+    else if ((waluta == "PLN" && waluta1 == "USD"))
+        document.getElementById("wynik").innerHTML = liczba + waluta + " = " + liczba / dolar + waluta1;
+    else if (waluta == "USD" && waluta1 == "PLN")
+        document.getElementById("wynik").innerHTML = liczba + waluta + " = " + liczba * dolar + waluta1;
+    else if ((waluta == "PLN" && waluta1 == "EUR"))
+        document.getElementById("wynik").innerHTML = liczba + waluta + " = " + liczba / euro + waluta1;
+    else if (waluta == "EUR" && waluta1 == "PLN")
+        document.getElementById("wynik").innerHTML = liczba + waluta + " = " + liczba * euro + waluta1;
+    else if (waluta == "USD" && waluta1 == "EUR")
+        document.getElementById("wynik").innerHTML = liczba + waluta + " = " + liczba * dolar / euro + waluta1;
+    else if (waluta == "EUR" && waluta1 == "USD")
+        document.getElementById("wynik").innerHTML = liczba + waluta + " = " + liczba * euro / dolar + waluta1;
+}
